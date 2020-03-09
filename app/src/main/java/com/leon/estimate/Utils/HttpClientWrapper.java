95edbedf8bdcc3761a26609d3cbf28b02131442f
ali93angle <parkway33@ymail.com>
2020-03-09 10:43:30 +0330

    New Version

--- app/src/main/java/com/leon/estimate/Utils/HttpClientWrapper.java
+++ app/src/main/java/com/leon/estimate/Utils/HttpClientWrapper.java
@@ -4,6 +4,8 @@ package com.leon.estimate.Utils;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
 
 import com.leon.estimate.Enums.DialogType;
 import com.leon.estimate.Enums.ErrorHandlerType;
@@ -39,20 +41,34 @@ final public class HttpClientWrapper {
                 dialog.setCancelable(true);
             else dialog.setCancelable(false);
         }
-        final String[] error = new String[1];
-        call.enqueue(new Callback<T>() {
-            @Override
-            public void onResponse(Call<T> call, Response<T> response) {
-                try {
-                    if (response.isSuccessful()) {
-                        T responseT = response.body();
-                        callback.execute(responseT);
-                        dialog.dismiss();
-                    } else {
+        if (isOnline(context)) {
+            final String[] error = new String[1];
+            call.enqueue(new Callback<T>() {
+                @Override
+                public void onResponse(Call<T> call, Response<T> response) {
+                    try {
+                        if (response.isSuccessful()) {
+                            T responseT = response.body();
+                            callback.execute(responseT);
+                            dialog.dismiss();
+                        } else {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response.errorBody().string());
+                                error[0] = jsonObject.getString(context.getString(R.string.message));
+                            } catch (Exception e) {
+                                CustomErrorHandling customErrorHandling = new CustomErrorHandling(context);
+                                error[0] = customErrorHandling.getErrorMessage(response.code(), errorHandlerType);
+                            }
+                            new CustomDialog(DialogType.Yellow, context, error[0], context.getString(R.string.dear_user),
+                                    context.getString(R.string.error), context.getString(R.string.accepted));
+                            dialog.dismiss();
+                        }
+                    } catch (Exception e) {
+
                         try {
                             JSONObject jsonObject = new JSONObject(response.errorBody().string());
                             error[0] = jsonObject.getString(context.getString(R.string.message));
-                        } catch (Exception e) {
+                        } catch (Exception e1) {
                             CustomErrorHandling customErrorHandling = new CustomErrorHandling(context);
                             error[0] = customErrorHandling.getErrorMessage(response.code(), errorHandlerType);
                         }
@@ -60,33 +76,24 @@ final public class HttpClientWrapper {
                                 context.getString(R.string.error), context.getString(R.string.accepted));
                         dialog.dismiss();
                     }
-                } catch (Exception e) {
+                }
 
-                    try {
-                        JSONObject jsonObject = new JSONObject(response.errorBody().string());
-                        error[0] = jsonObject.getString(context.getString(R.string.message));
-                    } catch (Exception e1) {
+                @Override
+                public void onFailure(Call<T> call, Throwable t) {
+                    Activity activity = (Activity) context;
+                    if (!activity.isFinishing()) {
                         CustomErrorHandling customErrorHandling = new CustomErrorHandling(context);
-                        error[0] = customErrorHandling.getErrorMessage(response.code(), errorHandlerType);
+                        error[0] = customErrorHandling.getErrorMessageTotal(t);
+                        new CustomDialog(DialogType.Red, context, error[0], context.getString(R.string.dear_user),
+                                context.getString(R.string.error), context.getString(R.string.accepted));
                     }
-                    new CustomDialog(DialogType.Yellow, context, error[0], context.getString(R.string.dear_user),
-                            context.getString(R.string.error), context.getString(R.string.accepted));
                     dialog.dismiss();
                 }
-            }
-
-            @Override
-            public void onFailure(Call<T> call, Throwable t) {
-                Activity activity = (Activity) context;
-                if (!activity.isFinishing()) {
-                    CustomErrorHandling customErrorHandling = new CustomErrorHandling(context);
-                    error[0] = customErrorHandling.getErrorMessageTotal(t);
-                    new CustomDialog(DialogType.Red, context, error[0], context.getString(R.string.dear_user),
-                            context.getString(R.string.error), context.getString(R.string.accepted));
-                }
-                dialog.dismiss();
-            }
-        });
+            });
+        } else {
+            dialog.dismiss();
+            Toast.makeText(context, "اینترنت وصل نیست، اینترنت وایفای یا همراه خود را روشن کنید.", Toast.LENGTH_LONG).show();
+        }
     }
 
     public static <T> void callHttpAsync(Call<T> call, final ICallback callback, final ErrorHandlerType errorHandlerType) {
@@ -132,5 +139,10 @@ final public class HttpClientWrapper {
 //                Toast.makeText(context, error[0], Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private static boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnectedOrConnecting();
     }
 }
