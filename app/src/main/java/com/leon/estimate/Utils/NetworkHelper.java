74a7544b27fd71ee4053f71dbb488ccb91244d4b
Leon <ali.rostami33@gmail.com>
2020-11-18 13:30:09 +0330

    New Version gradle

--- app/src/main/java/com/leon/estimate/Utils/NetworkHelper.java
+++ app/src/main/java/com/leon/estimate/Utils/NetworkHelper.java
@@ -4,7 +4,7 @@ import android.content.Context;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.leon.estimate.Enums.CompanyNames;
+import com.leon.estimate.MyApplication;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -15,17 +15,18 @@ import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 /**
  * Created by Leon on 12/9/2017.
  */
 
 public final class NetworkHelper {
-    private static TimeUnit TIME_UNIT = TimeUnit.SECONDS;
-    private static long READ_TIMEOUT = 120;
-    private static long WRITE_TIMEOUT = 60;
-    private static long CONNECT_TIMEOUT = 10;
-    private static boolean RETRY_ENABLED = true;
+    private static final TimeUnit TIME_UNIT = TimeUnit.SECONDS;
+    private static final long READ_TIMEOUT = 120;
+    private static final long WRITE_TIMEOUT = 60;
+    private static final long CONNECT_TIMEOUT = 10;
+    private static final boolean RETRY_ENABLED = true;
 
     private NetworkHelper() {
 
@@ -34,32 +35,51 @@ public final class NetworkHelper {
     public static OkHttpClient getHttpClient(final String token) {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient
+
+        return new OkHttpClient
                 .Builder()
                 .readTimeout(READ_TIMEOUT, TIME_UNIT)
                 .writeTimeout(WRITE_TIMEOUT, TIME_UNIT)
                 .connectTimeout(CONNECT_TIMEOUT, TIME_UNIT)
                 .retryOnConnectionFailure(RETRY_ENABLED)
                 .addInterceptor(chain -> {
-                    Request request = chain.request().newBuilder().addHeader("Authorization", "Bearer " + token).build();
+                    Request request = chain.request().newBuilder().addHeader("Authorization",
+                            "Bearer " + token).build();
                     return chain.proceed(request);
                 })
                 .addInterceptor(interceptor).build();
-
-        return client;
     }
 
-    public static Retrofit getInstance(boolean b, String token) {
+    public static Retrofit getInstance(String token) {
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
         String baseUrl = DifferentCompanyManager.getBaseUrl(
                 DifferentCompanyManager.getActiveCompanyName());
-        if (!b)
-            baseUrl = DifferentCompanyManager.getLocalBaseUrl(CompanyNames.SEPANO);
+        if (!MyApplication.isLocal)
+            baseUrl = DifferentCompanyManager.getLocalBaseUrl(
+                    DifferentCompanyManager.getActiveCompanyName());
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .client(NetworkHelper.getHttpClient(token))
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        return retrofit;
+    }
+
+    public static Retrofit getInstance() {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        String baseUrl = DifferentCompanyManager.getBaseUrl(
+                DifferentCompanyManager.getActiveCompanyName());
+        if (!MyApplication.isLocal)
+            baseUrl = DifferentCompanyManager.getLocalBaseUrl(
+                    DifferentCompanyManager.getActiveCompanyName());
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(NetworkHelper.getHttpClient(""))
+                .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
         return retrofit;
