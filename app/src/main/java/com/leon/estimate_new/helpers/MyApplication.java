0de1ec223108f4b0cf2ced36b3d9532b506a798d
Ali Rostami <34213546+iamAliRostami@users.noreply.github.com>
2021-09-11 11:26:02 +0430

    newVersion initialize.

--- app/src/main/java/com/leon/estimate_new/helpers/MyApplication.java
+++ app/src/main/java/com/leon/estimate_new/helpers/MyApplication.java
@@ -20,13 +20,11 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Constants.appContext = getApplicationContext();
-        // Obtain the FirebaseAnalytics instance.
         FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         Bundle bundle = new Bundle();
         bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "id");
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "name");
         bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
-//        throw new RuntimeException("Test Crash"); // Force a crash
     }
 }
